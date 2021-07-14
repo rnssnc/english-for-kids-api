69620e4aea1b281ac23773abe0d474c7f66aa970
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-07-14 17:32:32 +0300

    fix: trying to fix cors

--- src/index.ts
+++ src/index.ts
@@ -24,11 +24,11 @@ cloudinaryV2.config({
 });
 
 app.use(cors());
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(Express.json()); // support json encoded bodies
 app.use(Express.urlencoded({ extended: true })); // support encoded bodies
 
